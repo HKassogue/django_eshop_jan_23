94967c2e577759d6dae4c520c2d538f34875c03a
Hamidou KASSOGUE <hamidoukass@gmail.com>
2023-01-17 19:47:02 +0000

    Add of category model

--- monapp/models.py
+++ monapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, null=False, blank=False, 
+        unique=True)
+    slug = models.CharField(max_length=35, null=True, blank=True, 
+        unique=True)
+    active = models.BooleanField(default=True, null=False, blank=False)
+    created_at = models.DateTimeField(null=False, blank=False, 
+        auto_now=True)
+    parent = models.ForeignKey('Category', null=True, 
+        on_delete=models.SET_NULL, related_name='subcategories')
