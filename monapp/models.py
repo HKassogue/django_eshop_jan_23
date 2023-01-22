4deb6ea2ee6ccad42ead8278fa0a873d29ce5af2
Dr Hamidou Kassogué <hamidoukass@gmail.com>
2023-01-22 17:54:12 +0000

    Add of image model with url update

--- monapp/models.py
+++ monapp/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
     active = models.BooleanField(default=True, null=False, blank=False)
     created_at = models.DateTimeField(null=False, blank=False, 
         auto_now=True)
-    parent = models.ForeignKey('Category', null=True, 
+    parent = models.ForeignKey('Category', null=True, blank=True,
         on_delete=models.SET_NULL, related_name='subcategories')
 
 
@@ -25,5 +25,13 @@ class Product(models.Model):
     active = models.BooleanField(default=True, null=False, blank=False)
     created_at = models.DateTimeField(null=False, blank=False, auto_now=True)
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, 
-        null=True)
-        
+        null=True, related_name='products')
+
+
+class Image(models.Model):
+    name = models.CharField(max_length=30, null=False, blank=False)
+    file = models.ImageField(upload_to='images/products/', null=False, 
+        blank=True)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, 
+        related_name='images')
+
