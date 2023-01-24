d6b0ea218e716377fd5285f94dee36853642eecf
Dr Hamidou Kassogué <hamidoukass@gmail.com>
2023-01-24 20:12:07 +0000

    making admin for all models with some features

--- monapp/admin.py
+++ monapp/admin.py
@@ -1,9 +1,32 @@
 from django.contrib import admin
+from django.apps import apps
 from .models import *
 
-admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Image)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'slug', 'parent']
+    search_fields = ['name', 'id']
+    list_filter = ['name']
+    prepopulated_fields = {'slug': ('name',), }
+    autocomplete_fields = ['parent']
+
+
+# admin.site.register(Alerts)
+# admin.site.register(Faqs)
+# admin.site.register(Order)
+# admin.site.register(Order_details)
+
+# all other models
+models = apps.get_models()
+
+for model in models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
 
 
 # Register your models here.
