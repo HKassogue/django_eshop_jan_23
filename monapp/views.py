a0e862626a23377a4ebdcbdd6563a5c3b1f20e13
Dr Hamidou Kassogué <hamidoukass@gmail.com>
2023-01-31 20:09:40 +0000

    Display of product details form DB and make url in home

--- monapp/views.py
+++ monapp/views.py
@@ -19,8 +19,16 @@ def checkout(request):
 def contact(request):
     return render(request, 'monapp/contact.html')
 
-def detail(request):
-    return render(request, 'monapp/detail.html')
+def detail(request, id):
+    product = Product.objects.get(id=id)
+    categories = Category.objects.all()
+    sim_products = Product.objects.filter(category=product.category).filter(active=True)
+    context = {
+        'categories': categories, 
+        'sim_products': sim_products,
+        'product': product
+    }
+    return render(request, 'monapp/detail.html', context)
 
 def shop(request):
     return render(request, 'monapp/shop.html')
